d4e5b7d04771e545c8fdff20dcdeb43b6b54a713
gokhan.akbas <gokhan.akbas@tigahealth.com>
2025-12-09 16:35:54 +0300

    feat: Initial project setup
    - Configure project with app and GCharts library modules.
    - Set up Gradle build scripts with Kotlin DSL.
    - Add basic Android application components including Manifest, themes, and launcher icons.
    - Add initial LineChart composable.
    - Include standard test classes and .gitignore files.

---
+++ GCharts/src/main/java/com/gkhnakbs/gcharts/charts/LineChart.kt
@@ -0,0 +1,13 @@
+package com.gkhnakbs.gcharts.charts
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+/**
+ * Created by Gökhan Akbaş on 09/12/2025.
+ */
+
+@Composable
+fun LineChart() {
+
+}
